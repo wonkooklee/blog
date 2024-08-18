47bd7e5e1a06f87d29e0baca43c84876ee8268e4
wonkooklee <wonkooklee@github.com>
2024-08-18 17:30:37 +0900

    post comment 추가

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/post-footer.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { useEffect, useRef } from "react";
+import "./post-footer.css";
+
+export default function PostFooter() {
+  const commentsEl = useRef<HTMLDivElement>(null);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (loaded) return;
+    const scriptEl = document.createElement("script");
+    scriptEl.async = true;
+    scriptEl.src = "https://utteranc.es/client.js";
+    scriptEl.setAttribute("repo", "wonkooklee/blog");
+    scriptEl.setAttribute("issue-term", "pathname");
+    scriptEl.setAttribute("theme", "preferred-color-scheme");
+    scriptEl.setAttribute("crossorigin", "anonymous");
+
+    if (commentsEl.current) {
+      commentsEl.current.appendChild(scriptEl);
+    }
+    setLoaded(true);
+  }, []);
+
+  return (
+    <div>
+      <div ref={commentsEl} />
+    </div>
+  );
+}
